bc69f5ebf634172a3af307ea91f1589866cff252
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2015-08-12 12:23:55 -0300

    celendars

--- app/views/navs/charts.blade.php
+++ app/views/navs/charts.blade.php
@@ -5,19 +5,19 @@ $patient = PatientUser::where('users_id',$user->id)->get()->first();
     <div class="container" style="padding-top: 15px;">
         <a href="/charts/{{$patient->patients_id}}/day" class="col-xs-4" style="text-align: center;">
             <div style="color: white">
-                <i class="fa fa-calendar fa-2x"></i><br>
+                <img src="/icon/diario-01.png" style="width: 15%;height: 15%;"><br>
                 <span>Diario</span>
             </div>
         </a>
         <a href="/charts/{{$patient->patients_id}}/week" class="col-xs-4" style="text-align: center;">
             <div style="color: white">
-                <i class="fa fa-calendar fa-2x"></i><br>
+                <img src="/icon/semanal-01.png" style="width: 15%;height: 15%;"><br>
                 <span>Semanal</span>
             </div>
         </a>
         <a href="/charts/{{$patient->patients_id}}/month" class="col-xs-4" style="text-align: center;">
             <div style="color: white">
-                <i class="fa fa-calendar fa-2x"></i><br>
+                <img src="/icon/mensual-01.png" style="width: 15%;height: 15%;"><br>
                 <span>Mensual</span>
             </div>
         </a>
