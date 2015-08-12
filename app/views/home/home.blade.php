aba97fed5a6fbfdcb524418a180a258278b14827
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2015-08-12 19:28:32 -0300

    home img

--- app/views/home/home.blade.php
+++ app/views/home/home.blade.php
@@ -1,8 +1,9 @@
 @extends('layout.master')
 
 @section('content')
-<div class="container">
+<div class="container" style="padding-right: 0px;padding-left: 0px;">
     @include('navs.top')
+    <img src="/img/cristal.jpg" style="width: 100%;padding-top: 50px;">
     @include('navs.bottom')
 
 </div>
