0809d422e7e7aed4438823efaf1a3ba66c3dc3ee
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2015-08-18 06:41:06 -0300

    force with html

---
+++ app/views/Specialists/indexhtml.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AutistBook</title>
+
+    <link rel="icon" type="image/ico" href="/img/favicon.ico" />
+
+
+    <link media="all" type="text/css" rel="stylesheet" href="/packages/bootstrap/dist/css/bootstrap.min.css">
+
+    <link media="all" type="text/css" rel="stylesheet" href="/packages/font-awesome/css/font-awesome.min.css">
+
+    <link media="all" type="text/css" rel="stylesheet" href="/packages/morrisjs/morris.css">
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="/packages/jquery/dist/jquery.js"></script>
+
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="/packages/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <script src="/packages/raphael/raphael-min.js"></script>
+
+    <script src="/packages/morrisjs/morris.min.js"></script>
+
+
+</head>
+<body>
+<div class="container">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container" style="padding-top: 15px;">
+            <a href="/specialists" class="col-xs-6" style="text-align: center;">
+                <div style="color: white">
+                    <img src="/icon/especialistas-01.png" style="width: 18%;height: 18%;"><br>
+                    <span>Especialistas</span>
+                </div>
+            </a>
+            <a href="/file/1" class="col-xs-6" style="text-align: center;">
+                <div style="color: white">
+                    <img src="/icon/ficha-01.png" style="width: 18%;height: 18%;"><br>
+                    <span>Ficha</span>
+                </div>
+            </a>
+        </div>
+    </nav>        <div class="row" style="margin-top: 11%;padding-bottom: 10px;">
+        <div style="background-color: #CF2E4D ;border-color: #CF2E4D; min-height: 60px">
+            <h3 style="color: white;  position: relative;  top: 50%;  transform: translateY(50%);margin-left:25px;font-size: 20px;">Especialistas</h3>
+        </div>
+        <table style="background-color: #f18c24;width: 100%;margin-top: -15px;">
+            <tr style="height: 62px;">
+                <td rowspan="2"><img src="/icon/fernando-01.png" style="margin-left:30px;height: 60px;width: 60px;font-size: 16px;"></td><td style="color: white;text-align: left" >Fernando Alvarez<br style="  margin: 4px 0;"><strong style="color: #691029;">Fonoudi&oacute;logo</strong></td><td rowspan="2"><a href="mailto:someone@example.com"><img src="/icon/icono_mail-01.png" style="height: 25%"></a> </td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr style="height: 62px;">
+                <td rowspan="2"><img src="/icon/xandra-01.png" style="margin-left:30px;height: 60px;width: 60px;font-size: 16px;"></td><td style="color: white;text-align: left" >Xandra Castro<br style="  margin: 4px 0;"><strong style="color: #691029;">Psic&oacute;logo</strong></td><td rowspan="2"><a href="mailto:someone@example.com"><img src="/icon/icono_mail-01.png" style="height: 25%"></a> </td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr style="height: 62px;">
+                <td rowspan="2"><img src="/icon/josefina-01.png" style="margin-left:30px;height: 60px;width: 60px;font-size: 16px;"></td><td style="color: white;text-align: left" >Josefina Claro<br style="  margin: 4px 0;"><strong style="color: #691029;">Trabajo Ocupacional</strong></td><td rowspan="2"><a href="mailto:someone@example.com"><img src="/icon/icono_mail-01.png" style="height: 25%"></a> </td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr style="height: 62px;">
+                <td rowspan="2"><img src="/icon/pablo-01.png" style="margin-left:30px;height: 60px;width: 60px;font-size: 16px;"></td><td style="color: white;text-align: left" >Pablo Sinn<br style="  margin: 4px 0;"><strong style="color: #691029;">Neur&oacute;logo</strong></td><td rowspan="2"><a href="mailto:someone@example.com"><img src="/icon/icono_mail-01.png" style="height: 25%"></a> </td>
+            </tr>
+            <tr>
+                <td></td>
+            </tr>
+            <tr style="height: 70px;">
+                <td style="width: 28%"><img src="/icon/agregarnuevo-01.png" style="margin-left:30px;height: 60px;width: 60px;"><td style="color: white;text-align: left" >Agregar Nuevo</td>
+            </tr>
+            <tr style="height: 70px;">
+
+            </tr>
+        </table>
+    </div>
+    <nav class="navbar navbar-inverse navbar-fixed-bottom">
+        <div class="container" style="padding-top: 15px;">
+            <a href="/home/1" class="col-xs-4" style="text-align: center;">
+                <div style="color: white">
+                    <img src="/icon/inicio-01.png" style="width: 30%;height: 30%;"><br>
+                    <span>Inicio</span>
+                </div>
+            </a>
+            <a href="/alerts/1" class="col-xs-4" style="text-align: center;">
+                <div style="color: white">
+                    <img src="/icon/alertas-01.png" style="width: 30%;height: 30%;"><br>
+                    <span>Alertas</span>
+                </div>
+            </a>
+            <a href="/dictionary" class="col-xs-4" style="text-align: center;">
+                <div style="color: white">
+                    <img src="/icon/diccionario-01.png" style="width: 30%;height: 30%;"><br>
+                    <span>Diccionario</span>
+                </div>
+            </a>
+        </div>
+    </nav>    </div>
+</body>
+</html>
