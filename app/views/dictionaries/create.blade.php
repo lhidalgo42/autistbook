3ebab7e702d25693a0bbe22723c86b12e3cfc372
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2015-08-18 05:22:27 -0300

    cambios

--- app/views/dictionaries/create.blade.php
+++ app/views/dictionaries/create.blade.php
@@ -6,12 +6,27 @@
     </div>
     <div class="container" style="background-color: rgb(105,17,42)">
         <input type="text" placeholder="Nombre de la Conducta" style="margin-top: 30px;width : 330px;">
-        <textarea name="area" style="width: 330px;margin-top: 30px;height: 200px;" placeholder="Descripcion"></textarea>
-        <div style="margin-top: 15px;">
-            <span style="color: white;"><img src="/icon/icono_adjuntar-01.png" style="width: 10%;"><span style="margin-left: 5px;">Adjuntar Archivo </span><button class="pull-right" style="background-color: rgb(240,140,35);border-color: rgb(240,140,35);color: white;">A&ntilde;adir</button></span>
+        <textarea name="area" style="width: 330px;margin-top: 30px;height: 240px;" placeholder="Descripcion"></textarea>
+        <div class="clearfix" id="panel" style="position:fixed;background-color: rgb(240,140,35);display: none;margin-top: -70px;padding-top: 5px;padding-bottom: 5px;width: 330px;">
+            <div style="color: white;" class="col-xs-4">
+                <img src="/icon/galeria-01.png" style="width: 40%;margin-left: 7px;"><br style="  margin: 5px 0;">Galeria
+            </div>
+            <div style="color: white;" class="col-xs-4">
+                <img src="/icon/foto-01.png" style="width: 40%;margin-left:30px;"><br style="  margin: 5px 0;"><span style="margin-left: 30px;">Foto</span>
+            </div>
+            <div style="color: white;" class="col-xs-4">
+                <img src="/icon/video-01.png" style="width: 40%;margin-left: 44px;"><br style="  margin: 5px 0;"><span style="margin-left: 42px;">Video</span>
+            </div>
         </div>
-        <div class="row" style="margin-top: 11%;padding-bottom: 10px;">
+        <div style="margin-top: 15px;padding-bottom: 30px;">
+            <span style="color: white;"><img src="/icon/icono_adjuntar-01.png" style="width: 10%;"><span style="margin-left: 5px;">Adjuntar Archivo </span><button class="pull-right" style="background-color: rgb(240,140,35);border-color: rgb(240,140,35);color: white;" id="add">A&ntilde;adir</button></span>
         </div>
+        <script>
+            $("#add").click(function(){
+                $(this).html('publicar');
+                $("#panel").css('display','block');
+            })
+        </script>
         @include('navs.bottom')
     </div>
 @stop
