e6a22203f17ba3a57afa670f75011e5bf2910cae
Leonardo Hidalgo <lhidalgo@alumnos.uai.cl>
2015-08-18 06:01:43 -0300

    fix alerts

--- app/views/alerts/index.blade.php
+++ app/views/alerts/index.blade.php
@@ -1,8 +1,10 @@
 @extends('layout.master')
 
 @section('content')
+    <img src="/img/alerts.jpg" style="height: 458px;margin-top: 56px;position: relative;width: 360px;">
     <div class="container">
         @include('navs.top2')
+
         @include('navs.bottom')
 
     </div>
